8ae4c6cbc598001f1f5b6ba89885c5f819960897
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-25 19:47:50 -0500

    Documentación del Código

---
+++ LogService/Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.escuelaing.arep.app"},{"l":"edu.escuelaing.arep.app.model"},{"l":"edu.escuelaing.arep.app.persistence"}];updateSearchResults();
