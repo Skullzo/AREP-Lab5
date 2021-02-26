8a0b6110e61c99936ebbc11b9cf16a60d9028aec
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-25 19:02:30 -0500

    Código Fuente

--- RoundRobin/src/main/java/edu/escuelaing/arep/app/App.java
+++ RoundRobin/src/main/java/edu/escuelaing/arep/app/App.java
@@ -1,5 +1,44 @@
 package edu.escuelaing.arep.app;
-
+import static spark.Spark.*;
+/**
+ * Clase principal encargada de llevar a cabo la ejecucion de todo el programa.
+ * @author  Alejandro Toro Daza
+ * @version 1.0.  (25 de Febrero del 2021) 
+ */
 public class App {
-
+    /**
+     * Metodo principal encargado de recibir peticiones GET y POST.
+     * @param args Argumentos que entran a la clase principal.
+     */
+    public static void main( String[] args ) {
+        port(getPort());
+        staticFileLocation("/static");
+        HttpClient client = new HttpClient();
+        get("/", (req, res) -> {
+            res.redirect( "index.html");
+            return null;
+        });
+        get("/results", (req, res) -> {
+            res.status(200);
+            res.type("application/json");
+            String resp = client.getMessage();
+            client.changeNumberServer();
+            return resp;
+        });
+        post("/results", (req, res) -> {
+            client.postMessage(req.body());
+            client.changeNumberServer();
+            return "";
+        });
+    }
+    /**
+     * Metodo encargado de ejecutar el programa de manera local con un puerto predeterminado.
+     * @return Puerto asignado por defecto para ejecutar el programa de forma local, que es 4567.
+     */
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
 }
--- RoundRobin/src/main/java/edu/escuelaing/arep/app/HttpClient.java
+++ RoundRobin/src/main/java/edu/escuelaing/arep/app/HttpClient.java
@@ -1,5 +1,43 @@
 package edu.escuelaing.arep.app;
-
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+/**
+ * Clase encargada de realizar la conexion HTTP con el cliente del programa.
+ * @author  Alejandro Toro Daza
+ * @version 1.0.  (25 de Febrero del 2021) 
+ */
 public class HttpClient {
-
+    private String[] ports = {":8001",":8002",":8003"};
+    private int nServer = 0;
+    private String url = "http://192.168.99.100";
+    /**
+     * Metodo encargado de realizar una peticion GET al LogService.
+     * @return String Cadena que contiene la peticion GET realizado.
+     * @throws UnirestException Excepcion que se arroja si no se encuentra ninguna peticion.
+     */
+    public String getMessage() throws UnirestException {
+        HttpResponse<String> apiResponse = Unirest.get(url+ports[nServer]+"/messages").asString();
+        System.out.println("Petición GET de "+url+ports[nServer]);
+        return apiResponse.getBody();
+    }
+    /**
+     * Metodo encargado de realizar una peticion POST al LogService.
+     * @param message Mensaje ingresado por el usuario que es asimismo ingresado a la base de datos.
+     * @return String Cadena que contiene la peticion POST que se realizo.
+     * @throws UnirestException Excepcion que se arroja si no se encuentra ningun mensaje.
+     */
+    public String postMessage(String message) throws UnirestException {
+        HttpResponse<String> apiResponse = Unirest.post(url+ports[nServer]+"/messages")
+                .body(message)
+                .asString();
+        System.out.println("Petición POST de "+url+ports[nServer]);
+        return apiResponse.getBody();
+    }
+    /**
+     * Metodo encargado de cambiar el valor de la variable nServer para cambiar entre los tres puertos disponibles del LogService.
+     */
+    public void changeNumberServer(){
+        nServer=(nServer + 1) % ports.length;
+    }
 }
