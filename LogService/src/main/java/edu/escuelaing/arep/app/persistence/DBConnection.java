a2e97d4240ea4808f70adf7cb34adeefa6a38677
Alejandro Toro Daza <skullzo@outlook.com>
2021-03-01 20:55:40 -0500

    Código Fuente

--- LogService/src/main/java/edu/escuelaing/arep/app/persistence/DBConnection.java
+++ LogService/src/main/java/edu/escuelaing/arep/app/persistence/DBConnection.java
@@ -20,7 +20,7 @@ public class DBConnection {
      * Metodo constructor de la clase DBConnection.
      */
     public DBConnection() {
-        uri = new MongoClientURI("mongodb+srv://Alejandro:1234@database.ymvuc.mongodb.net/test");
+        uri = new MongoClientURI("mongodb://Alejandro:1234@0.0.0.0:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=Database&authMechanism=SCRAM-SHA-1&3t.uriVersion=3");
         mongoClient = new MongoClient(uri);
     }
     /**
